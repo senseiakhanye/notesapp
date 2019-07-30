b1ba4203939b75bbbd79e575629d6c18a809afe5
Katleho <akhanye@student.fr.42>
2019-07-30 03:56:32 +0200

    used npm and created own modules

--- notes.js
+++ notes.js
@@ -1,3 +1,7 @@
 const fullName = require("./other.js");
+const validator = require('validator');
+// import validator from 'validator';
+// import fullName from './other.js';
 
-console.log("Fullname : " + fullName("Katleho", "Khanye"));
+// console.log("Fullname : " + fullName("Katleho", "Khanye"));
+console.log(validator.isEmail("kakhanye@gmail.com"));
