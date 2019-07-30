b68f1550031394375e24eaf954a3c6ed5e8f6e93
Katleho <akhanye@student.fr.42>
2019-07-30 04:40:34 +0200

    Using chalk

--- notes.js
+++ notes.js
@@ -1,7 +1,9 @@
 const fullName = require("./other.js");
+const chalk = require('chalk');
 const validator = require('validator');
 // import validator from 'validator';
 // import fullName from './other.js';
 
 // console.log("Fullname : " + fullName("Katleho", "Khanye"));
 console.log(validator.isEmail("kakhanye@gmail.com"));
+console.log(chalk.bold.green.inverse("Is it successful?"));
